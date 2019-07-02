804f9a556181dcfec0e27241369528527b29af1e
oyo <oy@OYO00058.local>
2019-07-02 20:46:48 +0800

    my first demo

---
+++ src/main/java/com/oyo/shen/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.oyo.shen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@SpringBootConfiguration
+public class DemoApplication {
+    public static void main(String[] args){
+        SpringApplication.run(DemoApplication.class);
+    }
+}
---
+++ src/main/java/com/oyo/shen/web/HelloController.java
@@ -0,0 +1,13 @@
+package com.oyo.shen.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(String name){
+        return "hello, ".concat(name);
+    }
+}
